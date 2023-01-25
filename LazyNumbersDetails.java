51c2987568ecfa24a2de187d18c6e67eb5494927
Andrei Alexandru <andrei.lysandr@gmail.com>
2023-01-25 20:17:15 +0200

    Added repository files

---
+++ LazyNumbersDetails.java
@@ -0,0 +1,130 @@
+public class LazyNumbersDetails {
+
+    // Campul obiectului
+    private int number;
+    private boolean primeCache;
+    private int primeCacheCounter;
+    private boolean perfectCache;
+    private int perfectCacheCounter;
+    private boolean magicCache;
+    private int magicCacheCounter;
+
+
+    // Starea obiectului
+    public LazyNumbersDetails (int number) { // Constructor pentru initializarea campului "number"
+        this.number = number;
+    }
+
+    public void updateNumber (int number) {  // Metoda pentru a putea actualiza valoarea campului "number"
+        this.number = number;
+        this.primeCacheCounter = 0;
+        this.perfectCacheCounter = 0;
+        this.magicCacheCounter = 0;
+    }
+
+    public boolean isPrime (int number) {  // Metoda pentru a verifica daca "number" este prim
+        if (primeCacheCounter == 0) {
+            return isPrimeAlgorithm(number);
+        } else {
+            return primeNumberCache();
+        }
+    }
+
+    private boolean isPrimeAlgorithm(int number) { // Algoritm pentru calcularea numarului prim
+        for (int i = 2; i < number / 2; i++) {
+                if (number % i == 0) {
+                    primeCacheCounter = 1;
+                    primeCache = false;
+                    return false;
+                }
+            }
+        primeCacheCounter = 1;
+        primeCache = true;
+        return true;
+    }
+
+    private boolean primeNumberCache () {  // Cache pentru metoda "isPrime"
+        cacheText();
+        return this.primeCache;
+    }
+
+    public boolean isPerfect (int number) {  // Metoda pentru a verifica daca "number" este perfect
+        if (perfectCacheCounter == 0) {
+            return isPerfectAlgorithm(number);
+        } else {
+            return perfectNumberCache();
+        }
+    }
+
+    private boolean isPerfectAlgorithm(int number) {
+        int perfectSum = 0;
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                perfectSum += i;
+            }
+        }
+        if (number == perfectSum + 1) {
+            perfectCacheCounter = 1;
+            perfectCache = true;
+            return true;
+        }
+        perfectCacheCounter = 1;
+        perfectCache = false;
+        return false;
+    }
+
+    private boolean perfectNumberCache () {  // Cache pentru metoda "isPerfect"
+        cacheText();
+        return this.perfectCache;
+    }
+
+    public boolean isMagic (int number) {  // Metoda pentru a verifica daca "number" este magic
+        if (magicCacheCounter == 0) {
+            return isMagicAlgorithm(number);
+        } else {
+            return magicNumberCache();
+        }
+    }
+
+    private boolean isMagicAlgorithm(int number) {
+        int oneDigitNumber = magicNumber(number);
+
+        if (oneDigitNumber == 3 || oneDigitNumber == 7 || oneDigitNumber == 9) {
+            magicCacheCounter = 1;
+            magicCache = true;
+            return true;
+        }
+        magicCacheCounter = 1;
+        magicCache = false;
+        return false;
+    }
+
+    private int magicNumber (int number) {  // Metoda face suma cifrelor lui "oneDigitNumber"
+        int sum = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit;
+
+            number /= 10;
+        }
+        if (sum >= 10) {
+            sum = magicNumber(sum);
+        }
+        return sum;
+    }
+
+    public boolean magicNumberCache () {  // Cache pentru metoda "isMagic"
+        cacheText();
+        return this.magicCache;
+    }
+
+    private void cacheText () {  // Text descriptiv pentru a arata ca rezultatul a fost stocat in cache.
+        System.out.println("Rezultatul a fost accesat din cache.");
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
---
+++ LazyNumbersTest.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class LazyNumbersTest {
+
+    private static void switchComands() {
+        System.out.println("\nComenzile dispoinibile sunt:"
+                + "\n\t- 1. Aflati daca numarul este prim."
+                + "\n\t- 2. Aflati daca numarul este perfect."
+                + "\n\t- 3. Aflati daca numarul este magic."
+                + "\n\t- 4. Introduceti un alt numar."
+                + "\n\t- 5. Iesiti din aplicatie.\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nIntrodu te rog un numar:");
+        LazyNumbersDetails lazyNumber = new LazyNumbersDetails(sc.nextInt());
+
+        int readNumber = lazyNumber.getNumber();
+        boolean stopCommand = false;
+
+        if(readNumber < 0) {
+            System.out.println("\nIntroduceti va rog un numar pozitiv!");
+            readNumber = sc.nextInt();
+            lazyNumber.updateNumber(readNumber);
+        }
+
+        switchComands();
+
+        while (!stopCommand) {
+            System.out.println("Va rog alegeti urmatoarea comanda\n");
+            int number = sc.nextInt();
+
+            System.out.println();
+
+            switch (number) {
+                case 1:
+                    System.out.println("Numarul introdus "
+                                       + (lazyNumber.isPrime(readNumber) ? "este prim!" : "nu este prim!"));
+                    break;
+                case 2:
+                    System.out.println("Numarul introdus "
+                                       + (lazyNumber.isPerfect(readNumber) ? "este perfect!" : "nu este perfect!"));
+                    break;
+                case 3:
+                    System.out.println("Numarul introdus " + (lazyNumber.isMagic(readNumber)
+                                       ? "este magic!" : "nu este magic."));
+                    break;
+                case 4:
+                    System.out.println("Introduceti noul numar:");
+                    readNumber = sc.nextInt();
+                    lazyNumber.updateNumber(readNumber);
+                    System.out.println("Numarul a fost actualizat. Noul numar este " + lazyNumber.getNumber() + "\n");
+                    break;
+                case 5:
+                    stopCommand = true;
+                    break;
+                default:
+                    System.out.println("Comanda nu este disponibila!");
+                    switchComands();
+                    break;
+            }
+        }
+        System.out.println("Va multumesc! \nLa revedere!");
+    }
+}
